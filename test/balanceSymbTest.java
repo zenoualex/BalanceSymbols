b78f9aed7d40881f2dff13d869660c7d7dff3835
Alexandros Papadimitriou <alexanderpapa7@gmail.com>
2018-01-26 16:05:22 +0200

    Create test folder and file for unit Testing

---
+++ test/balanceSymbTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class balanceSymbTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testBalancer() {
+		fail("Not yet implemented");
+	}
+
+}

9877bd271a7d5a3feaebfc33d1f26b02c3b3bc7d
Alexandros Papadimitriou <alexanderpapa7@gmail.com>
2018-01-27 15:33:01 +0200

    Imported Unit Testing with Junit and added some restrictions.

--- src/balanceSymb.java
+++ src/balanceSymb.java
@@ -1,41 +1,64 @@
 import java.util.Stack;
 
 public class balanceSymb {
-	public static char balancer(String s) {
-		//import class stack
-		Stack<Character> stack = new Stack<Character>();
-		//create loop for reading every char on string (s)
-		for (int i = 0; i < s.length(); i++) {
-			char firstcheck = s.charAt(i);
-			//Checks if Symbol matches with string
-			if (firstcheck == '(' || firstcheck == '[' || firstcheck == '{') {
-				//if yes register key to stack library
-				stack.push(firstcheck);
-			}
-			//Check if symbol closes in the string we gave
-			if (firstcheck == ')' || firstcheck == ']' || firstcheck == '}') {
-				char lastcheck = stack.peek();
-				//Checks if symbol also close
-				if (firstcheck == ')' && lastcheck == '(' || firstcheck == ']' && lastcheck == '['
-						|| firstcheck == '}' && lastcheck == '{') {
-					//if yes it removes from the list
-					stack.pop();
-				}else {
-					//If there are still symbols on stack library means that "Symbols are not balanced"
-					System.out.println("Is Not Balanced");
-					return 0;
-				}
+	public static String balancer(String s) {
+		boolean Matches = true;
+		//checks if input is empty
+		if (s.isEmpty() == true) {
+			System.out.println("String Is Empty please type some symbols.");
+			return "Input is empty";
+		}
+		//Checks that only symbols are allowed and not anything else
+		for (int counter = 0; counter < s.length() && Matches; counter++) {
+			char letter = s.charAt(counter);
+			if (letter != '(' && letter != ')' && letter != '{' && letter != '}' && letter != '[' && letter != ']') {
+				System.out.println("You can only type symbols like (, {, [");
+				Matches = false;
+				return "Only Symbols are allowed";
 			}
 		}
-		//Then if Stack library is empty all symbols are matches
-		System.out.println("Symbols Are Balanced");
-		return 0;
+		//if all input is valid continue
+		if (Matches) {
+			// import class stack
+			Stack<Character> stack = new Stack<Character>();
+			// create loop for reading every char on string (s)
+			for (int i = 0; i < s.length(); i++) {
+				char firstcheck = s.charAt(i);
+				// Checks if Symbol matches with string
+				if (firstcheck == '(' || firstcheck == '[' || firstcheck == '{') {
+					// if yes register key to stack library
+					stack.push(firstcheck);
+				}
+				// Check if symbol closes in the string we gave
+				if (firstcheck == ')' || firstcheck == ']' || firstcheck == '}') {
+					char lastcheck = stack.peek();
+					// Checks if symbol also close
+					if (firstcheck == ')' && lastcheck == '(' || firstcheck == ']' && lastcheck == '['
+							|| firstcheck == '}' && lastcheck == '{') {
+						// if yes it removes from the list
+						stack.pop();
+					} else {
+						// If there are still symbols on stack library means that "Symbols are not
+						// balanced"
+						System.out.println("Symbols Is Not Balanced");
+						return "Symbols are not Balanced";
+					}
+				}
+			}
+			// Then if Stack library is empty all symbols are matches
+			if (stack.isEmpty() == true && s.isEmpty() == false) {
+				System.out.println("Symbols Are Balanced");
+				return "Symbols Are Balanced";
+			}
+
+		}
+		return "";
 	}
 
 	public static void main(String[] args) {
-		//We import the string for the program to execute
-		String s = "[()()()]";
-		//calling the class balancer
+		// We import the string for the program to execute
+		String s = " ";
+		// calling the class balancer
 		balancer(s);
 	}
 }
--- test/balanceSymbTest.java
+++ test/balanceSymbTest.java
@@ -11,7 +11,28 @@ public class balanceSymbTest {
 
 	@Test
 	public void testBalancer() {
-		fail("Not yet implemented");
+		//Test cases
+		//Checks if the returns are for each case is similar with our test
+		String alpha = balanceSymb.balancer("[()]");
+		assertEquals("Symbols Are Balanced", alpha);
+		
+		String beta = balanceSymb.balancer("abg");
+		assertEquals("Only Symbols are allowed", beta);
+		
+		String beta1 = balanceSymb.balancer("123");
+		assertEquals("Only Symbols are allowed", beta1);
+		
+		String beta2 = balanceSymb.balancer("**");
+		assertEquals("Only Symbols are allowed", beta2);
+		
+		String delta3 = balanceSymb.balancer(" ");
+		assertEquals("Only Symbols are allowed", delta3);
+		
+		String delta = balanceSymb.balancer("");
+		assertEquals("Input is empty", delta);
+		
+		String epsilon = balanceSymb.balancer("{{)");
+		assertEquals("Symbols are not Balanced", epsilon);
 	}
 
 }
